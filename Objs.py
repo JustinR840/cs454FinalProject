06c9636a5c45d9c1e92e374ef181596881e3069a
Yolopanther <yolopanther1693@gmail.com>
2018-05-14 11:56:10 -0700

    another commit

--- Objs.py
+++ Objs.py
@@ -1,136 +1,224 @@
 import enum
 
+
 class direction(enum.IntEnum):
-    UP = 0
-    RIGHT = 1
-    DOWN = 2
-    LEFT = 3
+	UP = 0
+	RIGHT = 1
+	DOWN = 2
+	LEFT = 3
+
+
+class NFA:
+	def __init__(self, transition_table, states, m_input, state_map, input_map):
+		self.transition_table = transition_table
+		self.states = states
+		self.state_map = state_map
+		self.m_input = m_input
+		self.input_map = input_map
+
+
+class State:
+	def __init__(self, tip1, tip2, top_vert, bot_vert):
+		self.tip1 = tip1
+		self.tip2 = tip2
+		self.top_vert = top_vert
+		self.bot_vert = bot_vert
+
+	def __str__(self):
+		return self.tip1.__str__() + " " + self.tip2.__str__() + " " + self.top_vert.__str__() + " " + self.bot_vert.__str__()
+
 
 class TestObj:
-    def __init__(self):
-        self.state =\
-            [
-                #0  1  2
-                [0, 0, 0], # 0
-                [0, 0, 0], # 1
-                [0, 0, 0], # 2
-                [0, 0, 0], # 3
-                [0, 0, 0]  # 4
-            ]
-        self.tip1 = [0, 0]
-        self.tip2 = [0, 0]
-        #self.top_vert_used = False
-        #self.bot_vert_used = False
-        self.input_pair = [0, 0]
-        self.processed = 0
+	def __init__(self):
+		self.state = \
+			[
+				# 0  1  2
+				[0, 0, 0],  # 0
+				[0, 0, 0],  # 1
+				[0, 0, 0],  # 2
+				[0, 0, 0],  # 3
+				[0, 0, 0]  # 4
+			]
+		self.tip1 = [0, 0]
+		self.tip2 = [0, 0]
+		self.tip1_starting = [0, 0]
+		self.tip2_starting = [0, 0]
+		self.input_pair = [0, 0]
 
-    def isTopVertUsed(self):
-        return self.state[1][2] == 1
+	def LeftTopVertUsed(self):
+		return self.state[1][0] == 1
 
-    def isBotVertUsed(self):
-        return self.state[3][2] == 1
+	def LeftBotVertUsed(self):
+		return self.state[3][0] == 1
 
-    def pickSuitableNumbers(self):
+	def RightTopVertUsed(self):
+		return self.state[1][2] == 1
 
-        def numAdjacentEdges(state, starting):
-            num = 0
+	def RightBotVertUsed(self):
+		return self.state[3][2] == 1
 
-            if(state[starting[0] - 1][starting[1]] == 1): # up
-                num += 1
-            if(state[starting[0] + 1][starting[1]] == 1): # down
-                num += 1
-            if(state[starting[0]][starting[1] - 1] == 1): # left
-                num += 1
-            if(state[starting[0]][starting[1] + 1] == 1): # right
-                num += 1
+	def pickSuitableNumbers(self):
 
-            return num
+		def numAdjacentEdges(state, starting):
+			num = 0
 
+			if (state[starting[0] - 1][starting[1]] == 1):  # up
+				num += 1
+			if (state[starting[0] + 1][starting[1]] == 1):  # down
+				num += 1
+			if (state[starting[0]][starting[1] - 1] == 1):  # left
+				num += 1
+			if (state[starting[0]][starting[1] + 1] == 1):  # right
+				num += 1
 
-        self.input_pair[0] = numAdjacentEdges(self.state, [1, 1])
-        self.input_pair[1] = numAdjacentEdges(self.state, [3, 1])
+			return num
 
+		self.input_pair[0] = numAdjacentEdges(self.state, [1, 1])
+		self.input_pair[1] = numAdjacentEdges(self.state, [3, 1])
 
+	def getMoveableTipDirections(self, tip):
+		directions = []
+		if (tip[1] < len(self.state[0]) - 1 and self.state[tip[0]][tip[1] + 2] == 0):
+			directions.append(direction.RIGHT)
+		if (tip[1] > 0 and self.state[tip[0]][tip[1] - 2] == 0):
+			directions.append(direction.LEFT)
+		if (tip[0] < len(self.state) - 1 and self.state[tip[0] + 2][tip[1]] == 0):
+			directions.append(direction.DOWN)
+		if (tip[0] > 0 and self.state[tip[0] - 2][tip[1]] == 0):
+			directions.append(direction.UP)
 
-    def getMoveableTipDirections(self, tip):
-        directions = []
-        if(tip[1] < len(self.state[0]) - 1 and self.state[tip[0]][tip[1] + 2] == 0):
-            directions.append(direction.RIGHT)
-        if(tip[1] > 0 and self.state[tip[0]][tip[1] - 2] == 0):
-            directions.append(direction.LEFT)
-        if(tip[0] < len(self.state) - 1 and self.state[tip[0] + 2][tip[1]] == 0):
-            directions.append(direction.DOWN)
-        if(tip[0] > 0 and self.state[tip[0] - 2][tip[1]] == 0):
-            directions.append(direction.UP)
+		return directions
 
-        return directions
+	def setTip1(self, x, y):
+		self.state[y][x] = 1
+		self.tip1[0] = y
+		self.tip1[1] = x
+		self.tip1_starting[0] = y
+		self.tip1_starting[1] = x
 
-    def setTip1(self, x, y):
-        self.state[y][x] = 1
-        self.tip1[0] = y
-        self.tip1[1] = x
+	def setTip2(self, x, y):
+		self.state[y][x] = 1
+		self.tip2[0] = y
+		self.tip2[1] = x
+		self.tip2_starting[0] = y
+		self.tip2_starting[1] = x
 
-    def setTip2(self, x, y):
-        self.state[y][x] = 1
-        self.tip2[0] = y
-        self.tip2[1] = x
+	def moveTip1(self, dir):
+		if (dir == direction.UP):
+			if (self.tip1[0] > 0):
+				if (self.state[self.tip1[0] - 2][self.tip1[1]] == 0):
+					self.state[self.tip1[0] - 1][self.tip1[1]] = 1
+					self.state[self.tip1[0] - 2][self.tip1[1]] = 1
+					self.tip1[0] -= 2
+					# print("moved up")
+					return True
+				else:
+					# print("can't move up: direction already taken")
+					return False
+			else:
+				# print("can't move up: already at top")
+				return False
+		elif (dir == direction.RIGHT):
+			if (self.tip1[1] < len(self.state[0]) - 1):
+				if (self.state[self.tip1[0]][self.tip1[1] + 2] == 0):
+					self.state[self.tip1[0]][self.tip1[1] + 1] = 1
+					self.state[self.tip1[0]][self.tip1[1] + 2] = 1
+					self.tip1[1] += 2
+					# print("moved right")
+					return True
+				else:
+					# print("can't move right: direction already taken")
+					return False
+			else:
+				# print("can't move right: already at rightmost edge")
+				return False
+		elif (dir == direction.DOWN):
+			if (self.tip1[0] < len(self.state) - 1):
+				if (self.state[self.tip1[0] + 2][self.tip1[1]] == 0):
+					self.state[self.tip1[0] + 1][self.tip1[1]] = 1
+					self.state[self.tip1[0] + 2][self.tip1[1]] = 1
+					self.tip1[0] += 2
+					# print("moved down")
+					return True
+				else:
+					# print("can't move down: direction already taken")
+					return False
+			else:
+				# print("can't move down: already at bottom")
+				return False
+		elif (dir == direction.LEFT):
+			if (self.tip1[1] > 0):
+				if (self.state[self.tip1[0]][self.tip1[1] - 2] == 0):
+					self.state[self.tip1[0]][self.tip1[1] - 1] = 1
+					self.state[self.tip1[0]][self.tip1[1] - 2] = 1
+					self.tip1[1] -= 2
+					# print("moved left")
+					return True
+				else:
+					# print("can't move left: direction already taken")
+					return False
+			else:
+				# print("can't move left: already at leftmost edge")
+				return False
+		else:
+			Exception("Unknown direction")
 
-    def moveTip1(self, dir):
-        if(dir == direction.UP):
-            if(self.tip1[0] > 0):
-                if(self.state[self.tip1[0] - 2][self.tip1[1]] == 0):
-                    self.state[self.tip1[0] - 1][self.tip1[1]] = 1
-                    self.state[self.tip1[0] - 2][self.tip1[1]] = 1
-                    self.tip1[0] -= 2
-                    #print("moved up")
-                    return True
-                else:
-                    #print("can't move up: direction already taken")
-                    return False
-            else:
-                #print("can't move up: already at top")
-                return False
-        elif(dir == direction.RIGHT):
-            if(self.tip1[1] < len(self.state[0]) - 1):
-                if(self.state[self.tip1[0]][self.tip1[1] + 2] == 0):
-                    self.state[self.tip1[0]][self.tip1[1] + 1] = 1
-                    self.state[self.tip1[0]][self.tip1[1] + 2] = 1
-                    self.tip1[1] += 2
-                    #print("moved right")
-                    return True
-                else:
-                    #print("can't move right: direction already taken")
-                    return False
-            else:
-                #print("can't move right: already at rightmost edge")
-                return False
-        elif(dir == direction.DOWN):
-            if(self.tip1[0] < len(self.state) - 1):
-                if(self.state[self.tip1[0] + 2][self.tip1[1]] == 0):
-                    self.state[self.tip1[0] + 1][self.tip1[1]] = 1
-                    self.state[self.tip1[0] + 2][self.tip1[1]] = 1
-                    self.tip1[0] += 2
-                    #print("moved down")
-                    return True
-                else:
-                    #print("can't move down: direction already taken")
-                    return False
-            else:
-                #print("can't move down: already at bottom")
-                return False
-        elif(dir == direction.LEFT):
-            if(self.tip1[1] > 0):
-                if(self.state[self.tip1[0]][self.tip1[1] - 2] == 0):
-                    self.state[self.tip1[0]][self.tip1[1] - 1] = 1
-                    self.state[self.tip1[0]][self.tip1[1] - 2] = 1
-                    self.tip1[1] -= 2
-                    #print("moved left")
-                    return True
-                else:
-                    #print("can't move left: direction already taken")
-                    return False
-            else:
-                #print("can't move left: already at leftmost edge")
-                return False
-        else:
-            Exception("Unknown direction")
+	def moveTip2(self, dir):
+		if (dir == direction.UP):
+			if (self.tip2[0] > 0):
+				if (self.state[self.tip2[0] - 2][self.tip2[1]] == 0):
+					self.state[self.tip2[0] - 1][self.tip2[1]] = 1
+					self.state[self.tip2[0] - 2][self.tip2[1]] = 1
+					self.tip2[0] -= 2
+					# print("moved up")
+					return True
+				else:
+					# print("can't move up: direction already taken")
+					return False
+			else:
+				# print("can't move up: already at top")
+				return False
+		elif (dir == direction.RIGHT):
+			if (self.tip2[1] < len(self.state[0]) - 1):
+				if (self.state[self.tip2[0]][self.tip2[1] + 2] == 0):
+					self.state[self.tip2[0]][self.tip2[1] + 1] = 1
+					self.state[self.tip2[0]][self.tip2[1] + 2] = 1
+					self.tip2[1] += 2
+					# print("moved right")
+					return True
+				else:
+					# print("can't move right: direction already taken")
+					return False
+			else:
+				# print("can't move right: already at rightmost edge")
+				return False
+		elif (dir == direction.DOWN):
+			if (self.tip2[0] < len(self.state) - 1):
+				if (self.state[self.tip2[0] + 2][self.tip2[1]] == 0):
+					self.state[self.tip2[0] + 1][self.tip2[1]] = 1
+					self.state[self.tip2[0] + 2][self.tip2[1]] = 1
+					self.tip2[0] += 2
+					# print("moved down")
+					return True
+				else:
+					# print("can't move down: direction already taken")
+					return False
+			else:
+				# print("can't move down: already at bottom")
+				return False
+		elif (dir == direction.LEFT):
+			if (self.tip2[1] > 0):
+				if (self.state[self.tip2[0]][self.tip2[1] - 2] == 0):
+					self.state[self.tip2[0]][self.tip2[1] - 1] = 1
+					self.state[self.tip2[0]][self.tip2[1] - 2] = 1
+					self.tip2[1] -= 2
+					# print("moved left")
+					return True
+				else:
+					# print("can't move left: direction already taken")
+					return False
+			else:
+				# print("can't move left: already at leftmost edge")
+				return False
+		else:
+			Exception("Unknown direction")
--- main.py
+++ main.py
@@ -4,117 +4,218 @@ import copy
 
 
 def main():
-    allobjs = []
-    newobjs = []
+	allobjs = []
+	newobjs = []
+	processedojs = []
+
+	makeTestStates(allobjs)
+
+	stillIterating = True
+	while (stillIterating):
+		for obj in allobjs:
+			tip1_moveable_dirs = obj.getMoveableTipDirections(obj.tip1)
+
+			for dir in tip1_moveable_dirs:
+				newobj = copy.deepcopy(obj)
+				newobj.moveTip1(dir)
+				newobjs.append(newobj)
+			# tip2_moveable_dirs = obj.getMoveableTipDirections(obj.tip2)
+			# for dir in tip2_moveable_dirs:
+			# 	newobj = copy.deepcopy(obj)
+			# 	newobj.moveTip2(dir)
+			# 	newobjs.append(newobj)
+
+			obj.pickSuitableNumbers()
+			# Put processed objects in a different list so we won't process them in the next iteration
+			processedojs.append(obj)
+
+		# Set the next iteration to process the NEW objects
+		allobjs = newobjs
+
+		if (len(allobjs) == 0):
+			stillIterating = False
+		else:
+			# Clear the new objects list for the next iteration
+			newobjs = []
+
+	# processedobjs contains all final objects, set it to allobjs
+	allobjs = processedojs
 
 
-    objtm = TestObj()
-    objtm.setTip1(0, 0)
-    objtm.setTip2(0, 2)
-    allobjs.append(objtm)
+	# Create states
+	states = []
+	states_map = {}
 
-    objtb = TestObj()
-    objtb.setTip1(0, 0)
-    objtb.setTip2(0, 4)
-    allobjs.append(objtb)
+	count = 0
+	for obj in allobjs:
+		in_state = State(obj.tip1_starting, obj.tip2_starting, obj.LeftTopVertUsed(), obj.LeftBotVertUsed())
+		out_state = State(obj.tip1, obj.tip2, obj.LeftTopVertUsed(), obj.LeftBotVertUsed())
 
-    objtn = TestObj()
-    objtn.setTip1(0, 0)
-    # objtn.setTip2(0, 2)
-    allobjs.append(objtn)
-
-
-    # objmt = TestObj()
-    # objmt.setTip1(0, 2)
-    # objmt.setTip2(0, 0)
-    # allobjs.append(objmt)
-
-    objmb = TestObj()
-    objmb.setTip1(0, 2)
-    objmb.setTip2(0, 4)
-    allobjs.append(objmb)
-
-    objmn = TestObj()
-    objmn.setTip1(0, 2)
-    # objtn.setTip2(0, 2)
-    allobjs.append(objmn)
-
-
-    # objbt = TestObj()
-    # objbt.setTip1(0, 4)
-    # objbt.setTip2(0, 0)
-    # allobjs.append(objbt)
-
-    # objbm = TestObj()
-    # objbm.setTip1(0, 4)
-    # objbm.setTip2(0, 2)
-    # allobjs.append(objbm)
-
-    objbn = TestObj()
-    objbn.setTip1(0, 4)
-    # objtn.setTip2(0, 2)
-    allobjs.append(objbn)
+		if(not areStatesSame(in_state, out_state)):
+			if(not doesStateExists(in_state, states)):
+				states.append(in_state)
+				states_map[in_state.__str__()] = count
+				count += 1
+			else:
+				pass
+				#print("state already exists")
+			if(not doesStateExists(out_state, states)):
+				states.append(out_state)
+				states_map[out_state.__str__()] = count
+				count += 1
+			else:
+				pass
+				#print("state already exists")
+		else:
+			#print("states are same")
+			if(not doesStateExists(in_state, states)):
+				states.append(in_state)
+				states_map[in_state.__str__()] = count
+				count += 1
+			else:
+				pass
+				#print("state already exists")
 
 
-    # objnt = TestObj()
-    # # objnt.setTip1(0, 2)
-    # objnt.setTip2(0, 0)
-    # allobjs.append(objnt)
+	alphabet = [-1, 0, 1, 2, 3]
+	input_pairs = []
+	input_pairs_map = {}
 
-    # objnm = TestObj()
-    # # objnm.setTip1(0, 2)
-    # objnm.setTip2(0, 2)
-    # allobjs.append(objnm)
+	count = 0
+	for i in alphabet:
+		for j in alphabet:
+			input_pairs.append([i, j])
+			input_pairs_map[[i, j].__str__()] = count
+			count += 1
 
-    # objnb = TestObj()
-    # # objnb.setTip1(0, 2)
-    # objtn.setTip2(0, 4)
-    # allobjs.append(objnb)
+	transition_table = [[[] for x in range(len(input_pairs))] for y in range(len(states))]
 
-    objnn = TestObj()
-    # objnn.setTip1(0, 2)
-    # objtn.setTip2(0, 2)
-    allobjs.append(objnn)
+	nfa = NFA(transition_table, states, input_pairs, states_map, input_pairs_map)
 
+	for obj in allobjs:
+		in_state = State(obj.tip1_starting, obj.tip2_starting, obj.LeftTopVertUsed(), obj.LeftBotVertUsed())
+		out_state = State(obj.tip1, obj.tip2, obj.LeftTopVertUsed(), obj.LeftBotVertUsed())
 
-
-
-
-
-
-    # testobj = TestObj()
-    # testobj.setTip1(0, 4)
-    # testobj.setTip2(0, 2)
-    #
-    # allobjs.append(testobj)
-
-    stillIterating = True
-    while(stillIterating):
-        for obj in allobjs:
-            if(obj.processed == 0):
-                moveable_dirs = obj.getMoveableTipDirections(obj.tip1)
-                for dir in moveable_dirs:
-                    newobj = copy.deepcopy(obj)
-                    newobj.moveTip1(dir)
-                    newobjs.append(newobj)
-
-                obj.pickSuitableNumbers()
-                obj.processed = 1
-
-        if(len(newobjs) == 0):
-            stillIterating = False
-        else:
-            for obj in newobjs:
-                allobjs.append(obj)
-            newobjs = []
+		for i in range(len(nfa.states)):
+			pass
 
 
 
 
-    print()
 
 
 
 
+	print()
+
+def areStatesSame(state1, state2):
+	# for i in range(len(state1.state)):
+	# 	for j in range(len(state1.states[i])):
+	# 		if(state1.state[i][j] != state2.state[i][j]):
+	# 			return False
+
+	# tip1 endings in different spots
+	if(state1.tip1[0] != state2.tip1[0] or state1.tip1[1] != state2.tip1[1]):
+		return False
+
+	# tip2 endings in different spots
+	if (state1.tip2[0] != state2.tip2[0] or state1.tip2[1] != state2.tip2[1]):
+		return False
+
+	# Reversed tips
+	# if((state1.tip1[0] != state2.tip2[0] or state1.tip1[1] != state2.tip2[1]) and
+	# 	   (state1.tip2[0] != state2.tip1[0] or state1.tip2[1] != state2.tip1[1])):
+	# 	return False
+
+	# top/bot verticals not the same
+	if(state1.top_vert != state2.top_vert or state2.bot_vert != state2.bot_vert):
+		return False
+
+	# if(state1.tip1_starting[0] != state2.tip1_starting[0] or state1.tip1_starting[1] != state2.tip1_starting[1]):
+	# 	return False
+	#
+	# if(state1.tip2_starting[0] != state2.tip2_starting[0] or state2.tip1_starting[1] != state2.tip2_starting[1]):
+	# 	return False
+
+	return True
+
+def doesStateExists(state, state_list):
+	for i in state_list:
+		if(areStatesSame(state, i)):
+			return True
+
+	return False
+
+def makeTestStates(allobjs):
+	objtm = TestObj()
+	objtm.setTip1(0, 0)
+	objtm.setTip2(0, 2)
+	allobjs.append(objtm)
+
+	objtb = TestObj()
+	objtb.setTip1(0, 0)
+	objtb.setTip2(0, 4)
+	allobjs.append(objtb)
+
+	objtn = TestObj()
+	objtn.setTip1(0, 0)
+	# objtn.setTip2(0, 2)
+	allobjs.append(objtn)
+
+	# objmt = TestObj()
+	# objmt.setTip1(0, 2)
+	# objmt.setTip2(0, 0)
+	# allobjs.append(objmt)
+
+	objmb = TestObj()
+	objmb.setTip1(0, 2)
+	objmb.setTip2(0, 4)
+	allobjs.append(objmb)
+
+	objmn = TestObj()
+	objmn.setTip1(0, 2)
+	# objtn.setTip2(0, 2)
+	allobjs.append(objmn)
+
+	# objbt = TestObj()
+	# objbt.setTip1(0, 4)
+	# objbt.setTip2(0, 0)
+	# allobjs.append(objbt)
+
+	# objbm = TestObj()
+	# objbm.setTip1(0, 4)
+	# objbm.setTip2(0, 2)
+	# allobjs.append(objbm)
+
+	objbn = TestObj()
+	objbn.setTip1(0, 4)
+	# objtn.setTip2(0, 2)
+	allobjs.append(objbn)
+
+	# objnt = TestObj()
+	# # objnt.setTip1(0, 2)
+	# objnt.setTip2(0, 0)
+	# allobjs.append(objnt)
+
+	# objnm = TestObj()
+	# # objnm.setTip1(0, 2)
+	# objnm.setTip2(0, 2)
+	# allobjs.append(objnm)
+
+	# objnb = TestObj()
+	# # objnb.setTip1(0, 2)
+	# objtn.setTip2(0, 4)
+	# allobjs.append(objnb)
+
+	objnn = TestObj()
+	# objnn.setTip1(0, 2)
+	# objtn.setTip2(0, 2)
+	allobjs.append(objnn)
+
+	# testobj = TestObj()
+	# testobj.setTip1(0, 4)
+	# testobj.setTip2(0, 2)
+	#
+	# allobjs.append(testobj)
 
 main()
